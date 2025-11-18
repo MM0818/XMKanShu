3b532ac725bd34f36e50a49e2485db1fbcc7f402
manman <m12345678_0818@qq.com>
2025-11-18 15:51:46 +0800

    差目录和章节更新

---
+++ app/src/main/java/com/xmkanshu/UI/SearchActivity.java
@@ -0,0 +1,88 @@
+package com.xmkanshu.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.hb.dialog.dialog.LoadingDialog;
+import com.xmkanshu.Adapter.SearchListAdapter;
+import com.xmkanshu.R;
+import com.xmkanshu.Reptile.SearchBook;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SearchActivity extends AppCompatActivity {
+    ListView listView;
+    private ArrayList<HashMap<String, String>> list=new ArrayList<>();
+    EditText editText;
+    TextView textView;
+    TextView tv_dearch_hinit;
+    String stringtmp;
+    LoadingDialog loadingDialog;
+    SearchListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        getSupportActionBar().hide();
+        listView = findViewById(R.id.listview_search);
+        editText=findViewById(R.id.edittext_search);
+        textView=findViewById(R.id.tv_search_btn);
+        tv_dearch_hinit=findViewById(R.id.tv_search_hinit);
+        tv_dearch_hinit.setVisibility(View.GONE);
+        loadingDialog= new LoadingDialog(SearchActivity.this);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringtmp=editText.getText().toString();
+                new SearchTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            }
+        });
+    }
+    public class SearchTask extends AsyncTask<Void,Integer,Boolean>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loadingDialog.setMessage("loading");
+            loadingDialog.setCancelable(true); // 是否可以按“返回键”消失
+            loadingDialog.setCanceledOnTouchOutside(false); // 点击加载框以外的区域
+            loadingDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            list= SearchBook.SearchBookEvent(stringtmp);
+            adapter = new SearchListAdapter(list,SearchActivity.this);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    loadingDialog.dismiss();
+                    if(list.size()==0)
+                    {
+                        tv_dearch_hinit.setVisibility(View.VISIBLE);
+                        listView.setVisibility(View.GONE);
+                    }else {
+                        tv_dearch_hinit.setVisibility(View.GONE);
+                        listView.setVisibility(View.VISIBLE);
+                        listView.setAdapter(adapter);
+                    }
+                }
+            },2000);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/xmkanshu/UI/SettingFragment.java
@@ -0,0 +1,199 @@
+package com.xmkanshu.UI;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.hb.dialog.dialog.LoadingDialog;
+import com.xmkanshu.R;
+import com.xmkanshu.Service.DownloadService;
+import com.xmkanshu.Tool.ApkVersionCodeUtils;
+import com.xmkanshu.Tool.HttpRequest;
+import com.xmkanshu.ViewUitl.RightAndLeftTextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+
+/**
+ * @author ZQZESS
+ * @date 12/8/2020-9:42 PM
+ * @file SettingFragment.java
+ * GitHub：https://github.com/zqzess
+ *不会停止运行的app不是好app w(ﾟДﾟ)w
+ */
+public class SettingFragment extends Fragment {
+    RightAndLeftTextView tv_update;
+    String versionName;
+    Context context;
+    String downurl="";
+    String verName="";
+    String updateinfo="";
+    AlertDialog.Builder mDialog;
+    MyReceiver receiver = new MyReceiver();
+    LoadingDialog loadingDialog;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_setting, container, false);
+        initView(view);
+        return view;
+    }
+    void initView(View view)
+    {
+        context=this.getActivity();
+        loadingDialog=new LoadingDialog(context);
+        versionName= ApkVersionCodeUtils.getVerName(context);
+        tv_update=(RightAndLeftTextView) view.findViewById(R.id.setting_update);
+        tv_update.setmLeftText("当前版本号:"+versionName);
+        tv_update.setmRightText("检查更新 >");
+        clickEvent();
+        regist();
+    }
+    void clickEvent()
+    {
+        tv_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    loadingDialog.setMessage("");
+                    loadingDialog.show();
+                    HttpRequest.postJSONObject("https://api.github.com/repos/zqzess/TDKanKan/releases/latest", new Response.Listener<JSONObject >() {
+
+                        @Override
+                        public void onResponse(JSONObject  jsonObject) {
+                            try {
+                                     verName=jsonObject.getString("tag_name");
+                                     updateinfo=jsonObject.getString("body");
+                                    if (jsonObject.getJSONArray("assets") != null && jsonObject.getJSONArray("assets").length() > 0) {
+                                        JSONArray Array = jsonObject.getJSONArray("assets");
+                                        for (int i = 0; i < Array.length(); i++) {
+                                            JSONObject object = Array.getJSONObject(i);
+                                            downurl=object.getString("browser_download_url");
+//                                            Log.d("downurl",verName+"->"+downurl);
+                                            loadingDialog.dismiss();
+                                            if (verName.equals(versionName))
+                                            {
+                                                Toast.makeText(context,"当前已是最新版本",Toast.LENGTH_SHORT).show();
+                                            }else
+                                            {
+                                                UpdateTipDialog(context,verName,downurl,updateinfo);
+                                            }
+                                        }
+                                    }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(context,"网络请求错误，获取更新失败",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    void UpdateTipDialog(final Context context, final String verName, final String downUrl, final String updateinfo)
+    {
+        mDialog = new AlertDialog.Builder(context);
+        mDialog.setTitle("土豆阅读又更新咯！");
+        mDialog.setMessage("最新版本:"+verName+"\n"+updateinfo);
+        mDialog.setPositiveButton("立即更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent serviceIntent = new Intent(context, DownloadService.class);
+                //将下载地址url放入intent中
+//                Log.d("downurl",downUrl);
+                serviceIntent.setData(Uri.parse(downUrl));
+                SettingFragment.this.getActivity().startService(serviceIntent);
+            }
+        });
+        //设置反面按钮
+        mDialog.setNegativeButton("下次再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        mDialog.setCancelable(false).create().show();
+    }
+
+    private void regist() {
+
+        IntentFilter intentFilter = new IntentFilter(DownloadService.BROADCAST_ACTION);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        LocalBroadcastManager.getInstance(context).registerReceiver(receiver, intentFilter);
+    }
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String data = intent.getStringExtra(DownloadService.EXTENDED_DATA_STATUS);
+//            Log.i("test", data);
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+//            Toast.makeText(context, "编号："+id+"的下载任务已经完成！", Toast.LENGTH_SHORT).show();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "TDkankan.apk");
+//                Uri apkUri = FileProvider.getUriForFile(context,
+//                        "com.tdkankan.FileProvider", new File(Environment.getExternalStorageDirectory() + "/TDkankan.apk"));
+                Uri apkUri = FileProvider.getUriForFile(context, "com.tdkankan.FileProvider", file);
+                Intent install = new Intent(Intent.ACTION_VIEW);
+                install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //添加这一句表示对目标应用临时授权该Uri所代表的文件
+                install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                install.setDataAndType(apkUri, "application/vnd.android.package-archive");
+                context.startActivity(install);
+
+            } else {
+                Intent install = new Intent(Intent.ACTION_VIEW);
+//                install.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/TDkankan.apk")), "application/vnd.android.package-archive");
+                intent.setDataAndType(Uri.fromFile(new File(Environment.DIRECTORY_DOWNLOADS, "TDkankan.apk")), "application/vnd.android.package-archive");
+                install.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(install);
+            }
+//            intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/TDkankan.apk")),
+//                    "application/vnd.android.package-archive");
+//            startActivity(intent);
+
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver);
+    }
+}
